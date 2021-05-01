d4bbe7bb10555658d08377c64f3f3bce8c77c9ae
Dattu Burle <dattuburle234@.com>
2021-05-02 04:54:59 +0530

    created tabs

--- todolist/src/components/Tabs.js
+++ todolist/src/components/Tabs.js
@@ -48,20 +48,6 @@ function Tabs({ todos, completeTodo, removeTodo, updateTodo , onSubmit}) {
             <h1>Completed Tasks</h1>
           </div>
   
-          <div
-            className={toggleState === 3 ? "content  active-content" : "content"}
-          >
-            <h2>Content 3</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
-              nostrum rerum laudantium totam unde adipisci incidunt modi alias!
-              Accusamus in quia odit aspernatur provident et ad vel distinctio
-              recusandae totam quidem repudiandae omnis veritatis nostrum
-              laboriosam architecto optio rem, dignissimos voluptatum beatae
-              aperiam voluptatem atque. Beatae rerum dolores sunt.
-            </p>
-          </div>
         </div>
       </div>
     );
